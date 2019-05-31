17246e3d6f9183f2fe734514b12a71f962ff88b7
ivanmarkovic1402 <39127443+ivanmarkovic1402@users.noreply.github.com>
2019-05-31 09:44:35 +0200

    Update header.php

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
     <head>
         <meta charset="utf-8">
 
-        <title>IM clinic patients</title>
+        <title>Putno osiguranje</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
         <script src="https://use.fontawesome.com/1f59a29ea5.js"></script>
 
